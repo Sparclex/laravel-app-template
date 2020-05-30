063215aff0587d820a36ed1a190162c6d0664ee3
Sparclex <silvan@kbuhl.ch>
2020-05-30 13:09:52 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
